f4f8afac42d0d97e823bef071aad8a20357fe86b
Marcus Vinicius Estevam Candido <marcus.v.e.candido@gmail.com>
2022-06-30 18:19:09 -0300

    indentation

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,28 +10,26 @@ public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
-		
+
 		System.out.print("Sexo: ");
 		char gender = sc.nextLine().charAt(0);
 		while (gender != 'M' && gender != 'F') {
 			System.out.print("Valor invalido de intrada, digitar F/M: ");
 			gender = sc.nextLine().charAt(0);
-		}		
+		}
 		System.out.print("Quantidade de cervejas: ");
 		int beer = sc.nextInt();
 		System.out.print("Quantidade de refrigerantes: ");
 		int softDrink = sc.nextInt();
 		System.out.print("Quantidade de espetinhos: ");
 		int barbecue = sc.nextInt();
-		
+
 		Bill bill = new Bill(gender, beer, barbecue, softDrink);
-		
+
 		System.out.println();
-		
+
 		System.out.println(bill.toString());
-		
-		
+
 		sc.close();
 	}
 
--- src/entities/Bill.java
+++ src/entities/Bill.java
@@ -1,14 +1,14 @@
 package entities;
 
 public class Bill {
-	
+
 	private char gender;
 	private int beer;
 	private int barbecue;
 	private int softDrink;
-	
+
 	public Bill() {
-		
+
 	}
 
 	public Bill(char gender, int beer, int barbecue, int softDrink) {
@@ -49,33 +49,32 @@ public class Bill {
 	public void setSoftDrink(int softDrink) {
 		this.softDrink = softDrink;
 	}
-	
+
 	public double feeding() {
 		double totalBeer = beer * 5.00;
 		double totalBarbecue = barbecue * 7.00;
 		double totalSoftDrink = softDrink * 3.00;
 		double totalFeeding = totalBarbecue + totalBeer + totalSoftDrink;
-		return totalFeeding; 
+		return totalFeeding;
 	}
-	
+
 	public double cover() {
 		if (feeding() < 30.0) {
 			return 4.0;
-		}
-		else {
+		} else {
 			return 0;
 		}
-			
+
 	}
-	
+
 	public double ticket() {
-		if (gender == 'F'){
+		if (gender == 'F') {
 			return 8.0;
 		} else {
 			return 10.0;
 		}
 	}
-	
+
 	public double total() {
 		double sumTotal = ticket() + cover() + feeding();
 		return sumTotal;
@@ -87,15 +86,13 @@ public class Bill {
 		sb.append("RELATÓRIO: \r\n");
 		sb.append(String.format("Consumo = R$ %.2f %n", feeding()));
 		if (cover() > 0) {
-			sb.append(String.format("Couvert = R$ %.2f %n", cover()));	
+			sb.append(String.format("Couvert = R$ %.2f %n", cover()));
 		} else {
 			sb.append("Isento de Couvert \r\n");
 		}
 		sb.append(String.format("Ingresso = = R$ %.2f %n%n", ticket()));
-		sb.append(String.format("Valor a pagar = R$ %.2f %n", total()));		
+		sb.append(String.format("Valor a pagar = R$ %.2f %n", total()));
 		return sb.toString();
 	}
-	
-	
-	
+
 }
